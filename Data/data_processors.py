87bcfc72344b7de52051d0618e08ee89ef7603a3
BOUALILILila <el_boualili@esi.dz>
2020-11-19 14:18:24 +0100

    return to passage level marking

--- Data/data_processors.py
+++ Data/data_processors.py
@@ -154,8 +154,58 @@ class PassageProcessor(DataProcessor):
                         set_name: str,
     ):
         tf_writer = tf.io.TFRecordWriter(f"{output_dir}/dataset_{set_name}.tf")
-        tsv_writer = open(f"{output_dir}/pairs_{set_name}.tsv", 'w')
+        # tsv_writer = open(f"{output_dir}/pairs_{set_name}.tsv", 'w')
         ids_writer = open(f"{output_dir}/query_pass_ids_{set_name}.tsv", 'w')
+        i_ids = 0
+
+        start_time = time.time()
+
+        print('Counting number of examples...')
+        num_lines = sum(1 for line in open(data_path, 'r'))
+        print('{} examples found.'.format(num_lines))
+
+        #
+        prev_did = None
+        all_pass = ''
+        pids = []
+        passages =[]
+        labels =[]
+        #
+        with open(data_path, 'r') as f:
+            for i, line in enumerate(f):
+                if i % 1000 == 0:
+                    time_passed = int(time.time() - start_time)
+                    print('Processed training set, line {} of {} in {} sec'.format(
+                        i, num_lines, time_passed))
+                    hours_remaining = (num_lines - i) * time_passed / (max(1.0, i) * 3600)
+                    print('Estimated hours remaining to write the training set: {}'.format(
+                        hours_remaining))
+                              
+                qid, pid, query, doc, label, len_gt_query = line.rstrip().split('\t')
+
+                q, p = marker.mark(query, doc)
+
+                # write tfrecord
+                i_ids = self.handle.write_eval_example(tf_writer, tokenizer,
+                             q, [p], [int(label)], ids_writer, i_ids, 
+                             qid, [pid], int(len_gt_query))
+                
+                #tsv_writer.write(f"{qid}\t{q}\t{pid}\t{p}\t{label}\t{len_gt_query}\n")
+        tf_writer.close()
+        # tsv_writer.close()
+        ids_writer.close()
+
+    def prepare_inference_dataset_doc_level(
+                        self,
+                        tokenizer: typing.Union[PreTrainedTokenizer, AutoTokenizer],
+                        marker: Marker,
+                        data_path: str, 
+                        output_dir: str,
+                        set_name: str,
+    ):
+        tf_writer = tf.io.TFRecordWriter(f"{output_dir}/dataset_{set_name}_doc_level_marking.tf")
+        tsv_writer = open(f"{output_dir}/pairs_{set_name}_doc_level_marking.tsv", 'w')
+        ids_writer = open(f"{output_dir}/query_pass_ids_{set_name}_doc_level_marking.tsv", 'w')
         i_ids = 0
 
         start_time = time.time()
