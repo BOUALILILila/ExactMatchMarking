fc6950f16b874c9e06fa58d4466619679d3f8a81
BOUALILILila <el_boualili@esi.dz>
2021-03-22 10:39:09 +0100

    indent

--- Data/data_utils.py
+++ Data/data_utils.py
@@ -551,23 +551,23 @@ class MsMarcoPassagePrep(TopKPrepFromRetriever):
         return sorted_run
 
     def convert_to_unicode(self, text):
-    """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
-    if six.PY3:
-        if isinstance(text, str):
-        return text
-        elif isinstance(text, bytes):
-        return text.decode("utf-8", "ignore")
+        """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
+        if six.PY3:
+            if isinstance(text, str):
+            return text
+            elif isinstance(text, bytes):
+            return text.decode("utf-8", "ignore")
+            else:
+            raise ValueError("Unsupported string type: %s" % (type(text)))
+        elif six.PY2:
+            if isinstance(text, str):
+            return text.decode("utf-8", "ignore")
+            elif isinstance(text, unicode):
+            return text
+            else:
+            raise ValueError("Unsupported string type: %s" % (type(text)))
         else:
-        raise ValueError("Unsupported string type: %s" % (type(text)))
-    elif six.PY2:
-        if isinstance(text, str):
-        return text.decode("utf-8", "ignore")
-        elif isinstance(text, unicode):
-        return text
-        else:
-        raise ValueError("Unsupported string type: %s" % (type(text)))
-    else:
-        raise ValueError("Not running on Python2 or Python 3?")
+            raise ValueError("Not running on Python2 or Python 3?")
 
     def convert_train_dataset(
             self,
